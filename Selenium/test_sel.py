9ba5bac073b1285fe1ae69e1cca56926c9b250ab
01111996 <e.gasykova@gmail.com>
2026-05-04 14:55:47 +0300

    Add files via upload

---
+++ Selenium/test_sel.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+LOGIN_URL = "https://the-internet.herokuapp.com/login"
+
+#создаю фикстуру:
+
+@pytest.fixture
+def sumple_user():
+    options = Options()
+    options.add_argument("--headless")  
+    options.add_argument("--no-sandbox")  
+    options.add_argument("--disable-dev-shm-usage")  
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+# успешная авторизация:
+    
+def test_successful_login(sumple_user):
+    sumple_user.get(LOGIN_URL)
+    sumple_user.find_element(By.ID, "login").send_keys("tomsmith")
+    sumple_user.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    sumple_user.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    success_message = sumple_user.find_element(By.CSS_SELECTOR, ".flash.success")
+    assert "Welcome to the Secure Area. When you are done click logout below." in success_message.text
+
+ # не успешная автоизация:
+
+def  test_unsuccessful_login(user):
+    user.get(LOGIN_URL)
+    user.find_element(By.ID, "login").send_keys("prosto1")
+    user.find_element(By.ID, "password").send_keys("nepassword")
+    user.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    error_message = user.find_element(By.CSS_SELECTOR, ".flash.error")
+    assert "This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages." in error_message.text
