a5a1acc001747a3df7e3363e5087c8c2b93e3f29
01111996 <e.gasykova@gmail.com>
2026-05-04 15:09:33 +0300

    Update test_sel.py

--- Selenium/test_sel.py
+++ Selenium/test_sel.py
@@ -21,7 +21,7 @@ def sumple_user():
     
 def test_successful_login(sumple_user):
     sumple_user.get(LOGIN_URL)
-    sumple_user.find_element(By.ID, "login").send_keys("tomsmith")
+    sumple_user.find_element(By.ID, "username").send_keys("tomsmith")
     sumple_user.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
     sumple_user.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
     success_message = sumple_user.find_element(By.CSS_SELECTOR, ".flash.success")
@@ -29,9 +29,9 @@ def test_successful_login(sumple_user):
 
  # не успешная автоизация:
 
-def  test_unsuccessful_login(user):
+def  test_unsuccessful_login(sumple_user):
     user.get(LOGIN_URL)
-    user.find_element(By.ID, "login").send_keys("prosto1")
+    user.find_element(By.ID, "username").send_keys("prosto1")
     user.find_element(By.ID, "password").send_keys("nepassword")
     user.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
     error_message = user.find_element(By.CSS_SELECTOR, ".flash.error")
